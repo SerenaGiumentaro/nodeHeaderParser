1e4cc780a45975e33b36d77314c9dda4d80acc21
Serena Giumentaro <serena.giumentaro@apuliasoft.com>
2024-05-21 09:01:33 +0200

    test passed

--- index.js
+++ index.js
@@ -24,6 +24,12 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req, res) =>{
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+
+  res.json({ ipaddress: req.ip, language: language, software: software});
+})
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
